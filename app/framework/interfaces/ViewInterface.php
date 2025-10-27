7f0fb13ba01c6f0d7574c060d2daf7521e48f421
Андрей <alisura@gmail.com>
2025-10-27 20:44:05 +0300

    удален лишний интерфейс

--- app/framework/interfaces/ViewInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// app/interfaces/ViewInterface.php
-
-interface ViewInterface {
-    public function render(string $templatePath, array $data = [], array $headers = [], 
-        $httpCode = 200, $replace = true): string;
-}
