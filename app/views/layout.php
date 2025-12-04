86c785a151043ddbf8e3945469e7547eb19a74e3
Андрей <alisura@gmail.com>
2025-12-04 18:52:58 +0300

    фавиконки перенесены в корень паблика

--- app/views/layout.php
+++ app/views/layout.php
@@ -14,16 +14,19 @@
     <?php endif ?>
 
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= asset('favicon/favicon-32x32.png') ?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= asset('favicon/favicon-16x16.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= asset('favicon/apple-touch-icon.png') ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= asset('favicon/android-chrome-192x192.png') ?>">
-    <link rel="icon" type="image/png" sizes="512x512" href="<?= asset('favicon/android-chrome-512x512.png') ?>">
+    <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png">
 
-    <link rel="icon" type="image/png" sizes="120x120" href="<?= asset('favicon/android-chrome-192x192.png') ?>">
+    <link rel="icon" type="image/png" sizes="120x120" href="/android-chrome-192x192.png">
+
+    <link rel="manifest" href="/site.webmanifest">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
